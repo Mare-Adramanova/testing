c5da6eb07670bf7aa6871e831c4f7d42e44ec91e
Mare-Adramanova <mareadramanova@gmail.com>
2022-03-16 13:48:25 +0100

    second commit

--- index.php
+++ index.php
@@ -1,36 +1,18 @@
 <?php
-// echo '<pre>';
-// print_r($_GET);
-// echo '</pre>';
+
 $default = [
     'controller'=>'product',
     'method'=>'getAllProducts',
     'id'=>''
 ];
-//$params = $_GET;
+
 $params = isset($_GET['parameters']) ?
           explode("/", $_GET['parameters']) :
           [];
 
 
 
- //print_r($params);
  
-// if(isset($params[0]) && $params[0] === 'api'){
-//     require_once('api/index.php');
-//     $api = new APIController;
-    
-//     $method = $params[1];
-
-//     require('includes/globals.php');
-//     require('includes/general_functions.php');
-
-//     if(method_exists($api, $method)){
-//         $api->$method($params);
-//     }
-
-//     die;
-// }
 if(count($params) == 0){
     $controller = $default['controller'];
         $method = $default['method'] ;
@@ -65,26 +47,21 @@ switch(count($params)){
 
 $filename_controller = 'controllers/'. ucfirst($controller).'.php';
 $filename_model = 'models/'. ucfirst($controller).'.php';
-//echo($filename_controller);
+
 if(file_exists($filename_controller) && file_exists($filename_model)){
 
     require('includes/globals.php');
-   // require('includes/general_functions.php');
-    
-    /////da se napravi aftentikacija treba da se kreira nov file i da se includne da se naoga vo includes i da se vika general_functions.php
-    // require_authentication() ? success() : reject();
-    
 
     require($filename_controller);
     require($filename_model);
     
     $object = new $controller;
-    //print_r($object);
+    
     if(method_exists($object, $method)){
         $object->$method($id);
     }
     else{
-        echo 'metodot ne postoi';
+        echo 'method does not exist';
     }
     
 }
