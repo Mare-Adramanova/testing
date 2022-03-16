8d47a0f7258b52da39961e88c91d0cb7bdcdb18a
Mare-Adramanova <mareadramanova@gmail.com>
2022-03-16 23:19:44 +0100

    id changed

--- view/product/create.php
+++ view/product/create.php
@@ -14,7 +14,7 @@
         <div class="d-flex justify-content-between mt-5">
           <h2>Product Add</h2>
           <div >
-            <input type="submit" class="btn btn-outline-primary" value="Save">
+            <input type="submit" class="btn btn-outline-primary" value="Save" id="product_form">
             <a href="<?=$canceled_url?>" class="btn btn-outline-warning">Cancel</a>
           </div>
         </div>
@@ -23,22 +23,22 @@
             
             <div class="form-group mb-3 mt-3">
                 <label for="SKU">SKU:</label>
-                <input type="text" id="formbox" name="sku" class="form-control" required>
+                <input type="text" id="sku" name="sku" class="form-control" required>
             </div>
             <div class="form-group mb-3">    
                 <label for="Name">Name:</label>
-                <input type="text" id="formbox" name="name" class="form-control" required>
+                <input type="text" id="name" name="name" class="form-control" required>
                 
             </div>
             <div class="form-group mb-3">
                 <label for="Price">Price:</label>
-                <input type="number" id="formbox" name="price" class="form-control" required>
+                <input type="number" id="price" name="price" class="form-control" required>
             </div>
             <div class="form-group mb-3">
                 <label>Type Switcher:</label>
-                <select id="type" name="productType" onChange="prodType(this.value);" class="form-control" required>
+                <select id="productType" name="productType" onChange="prodType(this.value);" class="form-control" required>
                     <option value="" >Choose Switcher</option>
-                    <option value="Disc">Disc</option>
+                    <option value="DVD">Disc</option>
                     <option value="Book">Book</option>
                     <option value="Furniture">Furniture</option>
                 </select>
@@ -46,7 +46,7 @@
                 <div class="fieldbox" id="disc_attributes">
                   <div class="form-group">
                     <label>Size</label>
-                    <input type="text" name="size" value="" class="form-control" placeholder="size">
+                    <input type="text" name="size" value="" class="form-control" placeholder="size" id="size">
                   </div>  
                   <p >
                     <?= "Pleace provide size in MB for DVD-disc";?>
@@ -56,7 +56,7 @@
                 <div class="fieldbox" id="book_attributes">
                   <div class="form-group">
                     <label>Weight</label>
-                    <input type="text" name="weight" value="" class="form-control" placeholder="weight">
+                    <input type="text" name="weight" value="" class="form-control" placeholder="weight" id="weight">
                   </div>  
                   <p >
                     <?= "Pleace provide weight in Kg";?>
@@ -66,15 +66,15 @@
                 <div class="fieldbox" id="furniture_attributes">
                 <div class="form-group">
                     <label>Height</label>
-                    <input type="text" name="height" class="form-control">
+                    <input type="text" name="height" class="form-control" id="height">
                   </div>
                   <div class="form-group">  
                     <label >Width</label>
-                    <input type="text" name="width" class="form-control">
+                    <input type="text" name="width" class="form-control" id="width">
                   </div>  
                   <div class="form-group">
                     <label>Length</label>
-                    <input type="text" name="length" class="form-control" >
+                    <input type="text" name="length" class="form-control" id="length">
                   </div>
                   <p >
                     <?= "Pleace provide dimensions in HxWxL format";?>
@@ -99,7 +99,7 @@
                 bookAttributes.style.display="none";
                 furnitureAttributes.style.display="none";
                 
-                if(prod=="Disc"){
+                if(prod=="DVD"){
                   discAttributes.style.display="block";
                   discAttributes.setAttribute("required", "required");
                   
